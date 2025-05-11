d17354a0145ceb67aaa772121ca40883cd0b7de3
JohnBatowei <aritronwebsites@gmail.com>
2025-05-11 02:37:22 +0100

    first commit

---
+++ dist/src/api/course-material/controllers/course-material.js
@@ -0,0 +1,7 @@
+"use strict";
+/**
+ * course-material controller
+ */
+Object.defineProperty(exports, "__esModule", { value: true });
+const strapi_1 = require("@strapi/strapi");
+exports.default = strapi_1.factories.createCoreController('api::course-material.course-material');
