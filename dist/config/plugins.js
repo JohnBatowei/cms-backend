b316a107224825e13a34ade7a49e3410e6d10866
JohnBatowei <aritronwebsites@gmail.com>
2025-05-13 00:34:19 +0100

    upgraded

--- dist/config/plugins.js
+++ dist/config/plugins.js
@@ -1,3 +1,14 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = () => ({});
+exports.default = () => ({
+    upload: {
+        config: {
+            provider: 'cloudinary',
+            providerOptions: {
+                cloud_name: process.env.CLOUDINARY_NAME,
+                api_key: process.env.CLOUDINARY_KEY,
+                api_secret: process.env.CLOUDINARY_SECRET,
+            },
+        },
+    },
+});
