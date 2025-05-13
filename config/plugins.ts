0317dfc9e0b1a9a8993a4098b5cf9aaf9a8f6790
JohnBatowei <aritronwebsites@gmail.com>
2025-05-13 18:08:39 +0100

    update

--- config/plugins.ts
+++ config/plugins.ts
@@ -6,9 +6,6 @@ export default () => ({
         cloud_name: process.env.CLOUDINARY_NAME,
         api_key: process.env.CLOUDINARY_KEY,
         api_secret: process.env.CLOUDINARY_SECRET,
-        default_transformations: [
-          [{ fetch_format: 'auto', quality: 'auto' }],
-        ],
       },
     },
   },
